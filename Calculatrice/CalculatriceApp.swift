bbc410899ee8e284164e92e509c806438bbfbd4f
Cheikh Tacko SECK <cheikhseck24@gmail.com>
2022-03-11 14:13:16 +0100

    Initial Commit

---
+++ Calculatrice/CalculatriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatriceApp.swift
+//  Calculatrice
+//
+//  Created by Cheikh SECK on 11/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculatrice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculatrice
+//
+//  Created by Cheikh SECK on 11/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
